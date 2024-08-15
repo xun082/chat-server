7be991c1d24b9fce31d89d413a503cdb518351c9
xun082 <2042204285@qq.com>
2024-08-15 18:19:26 +0800

    fix: 🧩 修改获取用户信息接口

--- src/main.ts
+++ src/main.ts
@@ -20,7 +20,7 @@ async function bootstrap() {
   await registerFastifyPlugin(app, fastifyMultipart);
 
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: 'http://localhost:8081',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
     allowedHeaders: 'Content-Type, Accept, Authorization',
